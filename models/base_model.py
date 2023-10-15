00f13eaab61f03c0c35ba3281a45975d16e6589e
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-10-15 16:48:01 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 This module contains the BaseModel class
 """
 from datetime import datetime
-from __init__ import storage
+from models.__init__ import storage
 import uuid
 
 
@@ -26,7 +26,7 @@ class BaseModel:
             self.id = uuid.uuid4()
             self.id = str(self.id)
             self.created_at = datetime.now()
-            storage.new(self.__dict__)
+            storage.new(self)
         else:
             for key in kwargs:
                 value = kwargs[key]
@@ -45,14 +45,17 @@ class BaseModel:
         """
         updates the attribute updated_at with the current datetime
         """
-        storage.save()
         self.updated_at = datetime.now()
+        storage.save()
+        
 
     def to_dict(self):
         """
         returns a dictionary of all keys/values of __dict__ of the instance
         """
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
+        if type(self.created_at) != str:
+            self.created_at = self.created_at.isoformat()
+        if type(self.updated_at) != str:
+            self.updated_at = self.updated_at.isoformat()
         self.__dict__["__class__"] = "BaseModel"
         return self.__dict__
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,7 @@
 Module containing the classs FileStorage
 """
 import json
-import os.path
-
+from os import path
 
 class FileStorage:
     """
@@ -19,26 +18,42 @@ class FileStorage:
         """
         sets in __objects the obj with key <obj class name>.id
         """
-        unique_id = type(obj).__name__ + obj.id
-        __objects[unique_id] = obj
+        unique_id = type(obj).__name__ + "." + obj.id
+        self.__objects[unique_id] = obj
 
     def all(self):
         """
         returns the dictionary __objects
         """
-        return __objects
+        return self.__objects
 
     def save(self):
         """
         serializes __objects to the JSON file (path: __file_path)
         """
-        with open(__file_path, "w", encoding="utf-8") as f:
-            json.dumps(__objects, f)
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r+") as f:
+                lines = f.read()
+                data = json.loads(lines)
+                for key, value in FileStorage.__objects.items():
+                    data[key] = value.to_dict()
+                json.dump(data, f)
+        with open(FileStorage.__file_path, "w+", encoding="utf-8") as f:
+            serialised_dict = {}
+            for key, value in FileStorage.__objects.items():
+                serialised_dict[key] = value.to_dict()
+            json.dump(serialised_dict, f)
 
     def reload(self):
         """
         deserializes the JSON file to __objects
         """
-        if os.path.exists(__file_path) = True:
-            with open(__file_path, "r", encoding="utf-8") as f:
-                __objects = json.load(f)
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as f:
+                lines = f.read()
+                data = json.loads(lines)
+                FileStorage.__objects = {}
+                for key, value in data.items():
+                    class_name, class_id = key.split(".")
+                    FileStorage.__objects[key] = value
+     
